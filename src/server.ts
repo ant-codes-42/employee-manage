dd797526499f20283863b22fc5af86aa61f8d067
ant-codes-42 <schwab.it@pm.me>
2025-01-30 11:35:11 -0600

    basic inquirer menu structure

--- src/server.ts
+++ src/server.ts
@@ -4,3 +4,79 @@ import inquirer from 'inquirer';
 
 await connectToDb();
 
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'mainMenu',
+            choices: [
+                'View',
+                'Add',
+                'Update',
+                'Delete',
+                'Quit'
+            ]
+        }
+    ])
+
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'viewMenu',
+            choices: [
+                'View all employees',
+                'View employees by manager',
+                'View employees by department',
+                'View all departments',
+                'View all roles',
+                'Back'
+            ]
+        }
+    ])
+
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'addMenu',
+            choices: [
+                'Add an employee',
+                'Add role',
+                'Add department',
+                'Back'
+            ]
+        }
+    ])
+
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'updateMenu',
+            choices: [
+                'Update employee role',
+                'Update employee manager',
+                'Back'
+            ]
+        }
+    ])
+
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'deleteMenu',
+            choices: [
+                'Delete an employee',
+                'Delete a role',
+                'Delete a department',
+                'Back'
+            ]
+        }
+    ])
